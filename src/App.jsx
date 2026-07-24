be80725e95bfd025a6cb5b2558c6f33006033e70
Victor-Olumide <173700324+Victor-Olumide@users.noreply.github.com>
2026-07-24 10:27:29 +0100

    page routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,23 @@ import Room from './pages/Room'
 import NotFound from './pages/NotFound'
 import Roommate from './pages/Roommate'
 import RoomDetails from './pages/RoomDetails'
+import Admin from './pages/Admin'
 
 export default function App() {
   return (
     <div className='overflow-x-hidden'>
     <Router>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/rooms' element={<Room />} />
         <Route path='/room/:hostel/:number' element={<RoomDetails />} />
         <Route path='/roommate' element={<Roommate />} />
+        <Route path='/admin' element={<Admin />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+
       <Footer />
     </Router>
     </div>
