e5635f8f91860099e07f64f6c7ef01c21feb9e2f
Victor-Olumide <173700324+Victor-Olumide@users.noreply.github.com>
2026-07-24 10:29:38 +0100

    feat: side bar navigation component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
+import { RiWhatsappFill } from "react-icons/ri";
+
 
 import { useState, useEffect } from "react";
 
@@ -32,13 +34,23 @@ export default function Sidebar() {
       />
     )}
 
-    <div className={`fixed top-0 left-0 h-screen w-97 bg-white text-blue-800 p-6 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out z-50 mt-18`}>
+    <div className={`fixed top-0 left-0 h-screen w-97 bg-white text-blue-800 p-6 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out z-50 my-18 flex flex-col min-h-screen overflow-y-auto`}>
         
-        <ul className="space-y-4">
+        <ul className="flex flex-col gap-2 space-y-4 h-1/3">
           <a href="/"><li className="hover:text-blue-200 cursor-pointer">Home</li></a>
             <a href="/rooms"><li className="hover:text-blue-200 cursor-pointer">Rooms</li></a>
-            <a href="/roommate"><li className="hover:text-blue-200 cursor-pointer">Roommate</li></a>
+            <a href="/roommate"><li className="hover:text-blue-200 cursor-pointer">All Details</li></a>
         </ul>
+
+        <div className="bg-blue-100 text-left p-4 mt-10 rounded-lg flex-flex-col gap-2">
+          Contact Developer
+          <a href="https://wa.me/+2349026023588" target="_blank" rel="noopener noreferrer">
+            <h2 className="text-green-500 cursor-pointer flex flex-row gap-1 items-center"> 
+              <RiWhatsappFill className="text-2xl" />
+              <p>WhatsApp</p>
+            </h2>
+          </a>
+        </div>
     </div>
     </div>
     </>
