9ff59d050cfec374ff48a86ad38c13b58abcd742
Victor-Olumide <173700324+Victor-Olumide@users.noreply.github.com>
2026-08-10 16:45:55 +0100

    fix: navbar glassy effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-40 w-full bg-white shadow-md px-4 py-3 sm:px-6 lg:px-8 text-blue-800 text-xs md:text-base flex items-center justify-between">
+    <header className="sticky top-0 z-40 w-full bg-white/70 backdrop-blur-xl border-b border-slate-200/60 shadow-sm px-4 py-3 sm:px-6 lg:px-8 text-blue-800 text-xs md:text-base flex items-center justify-between transition-all">
       {/* Sidebar Drawer Trigger */}
       <div className="flex items-center">
         <Sidebar />
@@ -13,6 +13,7 @@ export default function Navbar() {
         to="/" 
         className="flex flex-row items-center gap-2 hover:opacity-90 transition-opacity"
       >
+        
         <h1 className="font-bold text-xs sm:text-sm md:text-base tracking-wide text-right sm:text-left">
           AFE BABALOLA UNIVERSITY
         </h1>
